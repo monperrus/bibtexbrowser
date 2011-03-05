383cf79705cee67275d57610a98073b21b659ba9
Martin Monperrus <martin.monperrus@gmail.com>
2011-03-05 12:56:20 +0100

    prepared the class HTMLWrapper for usage with AJAX

--- bibtexbrowser.php
+++ bibtexbrowser.php
@@ -2488,6 +2488,9 @@ class HTMLWrapper {
  */
 function HTMLWrapper(&$content,$metatags=array()/* an array name=>value*/) {
 
+// when we load a page with AJAX
+// the HTTP header is taken into account, not the <meta http-equiv>
+header('Content-type: text/html; charset='.ENCODING);
 echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">'."\n";
 
 ?>
