3056b96caa071600701cbbb90c30730736eb61a2
Chris <chrisdavidramos@gmail.com>
2018-06-13 14:39:46 -0400

    server test will now properly display bibtexbrowser example

--- tests/router.php
+++ tests/router.php
@@ -1,8 +1,7 @@
 <?PHP
-// $_GET['all']=1;
 @define('BIBTEXBROWSER_DEFAULT_FRAME', 'all');
-//@define('BIBTEXBROWSER_DEFAULT_TEMPLATE','HTMLTemplate');
+@define('BIBTEXBROWSER_EMBEDDED_WRAPPER', 'HTMLTemplate');
 //
 $_GET['bib']='bibacid-utf8.bib';
 $_GET['wrapper']='BIBTEXBROWSER_EMBEDDED_WRAPPER';
-require( 'bibtexbrowser.php' );
+require('bibtexbrowser.php');
