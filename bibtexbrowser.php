148904a8fa1e1d018dd34ebb0bd11b825dfe7a8a
Martin Monperrus <monperrus@users.noreply.github.com>
2019-03-21 08:53:10 +0100

    Update bibtexbrowser.php

--- bibtexbrowser.php
+++ bibtexbrowser.php
@@ -1346,7 +1346,7 @@ class BibEntry {
     $extension = strtolower(pathinfo(parse_url($this->getField($bibfield),PHP_URL_PATH),PATHINFO_EXTENSION));
     switch ($extension) {
       // overriding the label if it's a known extension
-      case 'html': return $this->getLink($bibfield, $iconurl, 'pdf'); break;
+      case 'html': return $this->getLink($bibfield, $iconurl, 'html'); break;
       case 'pdf': return $this->getLink($bibfield, $iconurl, 'pdf'); break;
       case 'ps': return $this->getLink($bibfield, $iconurl, 'ps'); break;
       default:
