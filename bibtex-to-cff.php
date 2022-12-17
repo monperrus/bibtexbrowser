303d4e6a0712c07ef20b9df14e95f348e21acc87
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2022-12-17 19:40:25 +0100

    Minor: Improvement to bibtex-to-cff
    * Update usage info
    * Improve error message for invalid or missing id attribute

--- bibtex-to-cff.php
+++ bibtex-to-cff.php
@@ -1,10 +1,10 @@
 <?php
-// create CITATION.cff file for Github from a bibtex file
+// create CITATION.cff file for GitHub from a bibtex file
 // reference documentation: https://docs.github.com/en/repositories/managing-your-repositorys-settings-and-features/customizing-your-repository/about-citation-files#about-citation-files
 // script part of https://github.com/monperrus/bibtexbrowser/
 //
 // Usage
-// $ php bibtexbrowser-cff.php test_cli.bib --id classical 
+// $ php bibtex-to-cff.php test_cli.bib --id classical
 //     -> this creates a file CITATION.cff for @xx{classical,
 
 $_GET['library']=1;
@@ -14,12 +14,15 @@ function bibtexbrowser_cff($arguments) {
     $db = new BibDataBase();
     $db->load($arguments[1]);
     $current_entry=NULL;
-    $current_field=NULL;
     for ($i=2;$i<count($arguments); $i++) {
         $arg=$arguments[$i];
         if ($arg=='--id') {
             $current_entry = $db->getEntryByKey($arguments[$i+1]);
         }
+    }
+    if (is_null($current_entry)) {
+        echo "Did not find entry specified in the --id argument.";
+        return;
     }
     echo $current_entry->toCFF();
 }
